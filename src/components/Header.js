6a5cd040852e1c4666f2ff00c3f46d6828bfcdeb
aer1al <imranchowdy@gmail.com>
2020-09-27 14:56:28 +0100

    init

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <>
+      {/* MENU BUTTON */}
+      {/* LOGO */}
+      {/* SEARCH BOX */}
+      {/* 3 BUTTONS & CHECKOUT */}
+    </>
+  )
+}
+
+export default Header
