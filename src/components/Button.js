5b4e7122344667890ee03d0c63ddbd6d60acb922
aer1al <imranchowdy@gmail.com>
2020-09-27 17:24:20 +0100

    began work on Header

---
+++ src/components/Button.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Button.css'
+
+function Button({top, bottom}) {
+  return (
+    <>
+      <div className="button__container">
+        <span className="top">
+          {top}
+        </span>
+        <span className="bottom">
+          {bottom}
+        </span>
+      </div>
+    </>
+  )
+}
+
+export default Button
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,53 @@
-import React from 'react'
+import React, {useState} from 'react'
+import './Header.css'
+import menu from './images/menu.png'
+import logo from './images/logo.png'
+import search from  './images/search.png'
+import Button from './Button'
 
 function Header() {
+{/* ADD STATE FOR MENU DROPDOWN ON MENU CLICK */}
+
   return (
-    <>
-      {/* MENU BUTTON */}
-      {/* LOGO */}
-      {/* SEARCH BOX */}
-      {/* 3 BUTTONS & CHECKOUT */}
+    <> 
+      <div className="header">
+        
+        {/* MENU BUTTON */}
+        <div className="menu__container">
+          <img 
+            src={menu}
+            alt=""
+            className="menu__icon"/>
+        </div>
+
+        {/* LOGO */}
+        <div className="logo__container">
+          <img 
+            src={logo} 
+            alt=""
+            className="logo__icon"/>
+        </div>
+
+        {/* SEARCH BOX */}
+        <div className="search__container">
+          <input 
+            type="text" 
+            className="search__input"/>
+          <span className="search__iconContainer">
+          <img
+            src={search}
+            alt=""
+            className="search__icon"/>
+          </span>
+        </div>
+
+        {/* 3 BUTTONS & CHECKOUT */}
+        <div className="buttons__container">
+          <Button top="Hello, Sign In" bottom="Account & Lists" />
+          <Button top="Returns" bottom="& Orders" />
+          <Button top="Try" bottom="Prime" />
+        </div>
+      </div>
     </>
   )
 }
